b160a115f534b9f8938ff3796470d1cc69becde6
Arthur Yu <yuya008@outlook.com>
2017-06-17 07:01:27 +0800

    自旋锁

--- spinlock.go
+++ spinlock.go
@@ -7,38 +7,20 @@ import (
 	"sync/atomic"
 )
 
-const (
-	DefaultMaxSpinTimes = 100000
-)
-
 type SpinLocker struct {
 	v int32
-	MaxSpinTimes uint32
-	keeper *Keeper
 }
 
 func NewSpinLocker() *SpinLocker {
-	locker := &SpinLocker{
-		keeper: NewKeeper(),
-	}
-	return locker
+	return &SpinLocker{}
 }
 
 func (lock *SpinLocker) Lock() {
-	if lock.MaxSpinTimes == 0 {
-		lock.MaxSpinTimes = DefaultMaxSpinTimes
-	}
-	var curSpinTimes uint32
 	for {
-		curSpinTimes = 0
-		for ; curSpinTimes < lock.MaxSpinTimes; curSpinTimes++ {
-			if atomic.CompareAndSwapInt32(&lock.v, 0, 1) {
-				return
-			}
+		if atomic.CompareAndSwapInt32(&lock.v, 0, 1) {
+			return
 		}
-		lock.keeper.Wait()
 	}
-	return
 }
 
 func (lock *SpinLocker) TryLock() bool {
--- spinlock_test.go
+++ spinlock_test.go
@@ -1,14 +1,36 @@
 package golocker
 
-import "testing"
+import (
+	"testing"
+	"sync"
+)
 
 var spinLocker *SpinLocker
 
 func init() {
 	spinLocker = NewSpinLocker()
-	spinLocker.MaxSpinTimes = 100000
 }
 
 func TestSpinLocker(t *testing.T) {
+	var all int64
+	var wg sync.WaitGroup
+	wg.Add(10000000)
+	for i := 1; i <= 10000000; i++ {
+		go func() {
+			spinLocker.Lock()
+			all++
+			spinLocker.Unlock()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	t.Log(all)
+}
 
+func BenchmarkSpinLocker(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		spinLocker.Lock()
+		spinLocker.Unlock()
+	}
 }
